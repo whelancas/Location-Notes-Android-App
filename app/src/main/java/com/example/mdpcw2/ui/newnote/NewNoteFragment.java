d2fdaac166b349ac093c7617acd69ac408ec3241
Charles Mullen <psycm5@nottingham.ac.uk>
2024-01-14 11:40:36 +0000

    Added navigation to buttons in new note and new reminder fragments

--- app/src/main/java/com/example/mdpcw2/ui/newnote/NewNoteFragment.java
+++ app/src/main/java/com/example/mdpcw2/ui/newnote/NewNoteFragment.java
@@ -1,16 +1,20 @@
 package com.example.mdpcw2.ui.newnote;
 
+import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.mdpcw2.R;
 import com.example.mdpcw2.databinding.FragmentNewNoteBinding;
 
 public class NewNoteFragment extends Fragment {
@@ -28,4 +32,31 @@ public class NewNoteFragment extends Fragment {
         return root;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button useCurrentLocationButton = view.findViewById(R.id.newNoteButton);
+        Button cancelButton = view.findViewById(R.id.newNoteCancelButton);
+
+        useCurrentLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                useCurrentLocation(v);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_navigation_new_note_to_navigation_home);
+            }
+        });
+    }
+
+    private void useCurrentLocation(View v) {
+        // TODO: Database stuff
+
+        Navigation.findNavController(v).navigate(R.id.action_navigation_new_note_to_navigation_home);
+    }
 }
--- app/src/main/java/com/example/mdpcw2/ui/newreminder/NewReminderFragment.java
+++ app/src/main/java/com/example/mdpcw2/ui/newreminder/NewReminderFragment.java
@@ -1,16 +1,20 @@
 package com.example.mdpcw2.ui.newreminder;
 
+import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.mdpcw2.R;
 import com.example.mdpcw2.databinding.FragmentNewReminderBinding;
 
 public class NewReminderFragment extends Fragment {
@@ -26,6 +30,44 @@ public class NewReminderFragment extends Fragment {
         View root = binding.getRoot();
 
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button useCurrentLocationButton = view.findViewById(R.id.newReminderSetCurrentLocationButton);
+        Button useCustomLocationButton = view.findViewById(R.id.setReminderCustomLocationButton);
+        Button cancelButton = view.findViewById(R.id.newReminderCancelButton);
+
+        useCurrentLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                useCurrentLocation(v);
+            }
+        });
+
+        useCustomLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { useCustomLocation(v); }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { Navigation.findNavController(v).navigate(R.id.action_navigation_new_reminder_to_navigation_reminders); }
+        });
+    }
+
+    private void useCustomLocation(View v) {
+        // TODO: Database and location stuff
+
+        Navigation.findNavController(v).navigate(R.id.action_navigation_new_reminder_to_navigation_reminders);
+    }
+
+    private void useCurrentLocation(View v) {
+        // TODO: Database and location stuff
+
+        Navigation.findNavController(v).navigate(R.id.action_navigation_new_reminder_to_navigation_reminders);
     }
 
 }
